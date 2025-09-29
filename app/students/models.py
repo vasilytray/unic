243c89d4b5fbf296bba487acecb1e3d16ddcc6c4
vasilytry <vsankov@gmail.com>
2025-09-29 18:04:20 +0700

    Up

--- app/students/models.py
+++ app/students/models.py
@@ -1,28 +1,28 @@
 
-from sqlalchemy import Integer, String, Date, ForeignKey, Column
+from sqlalchemy import Integer, String, Date, ForeignKey, Column, text, Text
 from sqlalchemy.orm import relationship, Mapped, mapped_column
-from app.database import Base
+from app.database import Base, str_uniq, int_pk, str_null_true
 from datetime import date
 
 
 # создаем модель таблицы студентов
 class Student(Base):
-    student_id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True, autoincrement=True)
-    phone_number: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
+    id: Mapped[int_pk] #= mapped_column(Integer, primary_key=True, index=True, autoincrement=True)
+    phone_number: Mapped[str_uniq] #= mapped_column(String, unique=True, index=True, nullable=False)
     first_name: Mapped[str] = mapped_column(String, index=True, nullable=False)
     last_name: Mapped[str] = mapped_column(String, index=True, nullable=False)
     date_of_birth: Mapped[date] = mapped_column(Date, nullable=False)
-    email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
-    address: Mapped[str] = mapped_column(String, nullable=False)
+    email: Mapped[str_uniq] #= mapped_column(String, unique=True, nullable=False)
+    address: Mapped[str] = mapped_column(Text, nullable=False)
     enrollment_year: Mapped[int] = mapped_column(Integer, nullable=False)
     course: Mapped[int] = mapped_column(Integer, nullable=False)
-    special_notes: Mapped[str] = mapped_column(String, nullable=True)
-    major_id: Mapped[int] = mapped_column(Integer, ForeignKey("majors.major_id"), nullable=False)
+    special_notes: Mapped[str_null_true] #= mapped_column(String, nullable=True)
+    major_id: Mapped[int] = mapped_column(Integer, ForeignKey("majors.id"), nullable=False)
 
     major: Mapped["Major"] = relationship("Major", back_populates="students")
 
     def __str__(self):
-        return (f"{self.__class__.__name__}(student_id={self.student_id}, "
+        return (f"{self.__class__.__name__}(id={self.id}, "
                 f"first_name={self.first_name!r}, "
                 f"last_name={self.last_name!r})")
 
@@ -32,14 +32,15 @@ class Student(Base):
 
 # создаем модель таблицы факультетов (majors)
 class Major(Base):
-    major_id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True, autoincrement=True)
-    major_name: Mapped[str] = mapped_column(String, unique=True, nullable=False)
-    major_description: Mapped[str] = mapped_column(String, nullable=True)
+    id: Mapped[int_pk] #= mapped_column(Integer, primary_key=True, index=True, autoincrement=True)
+    major_name: Mapped[str_uniq] #= mapped_column(String, unique=True, nullable=False)
+    major_description: Mapped[str_null_true]#[str] = mapped_column(String, nullable=True)
 
-    students: Mapped[list["Student"]] = relationship("Student", back_populates="major")
+    #students: Mapped[list["Student"]] = relationship("Student", back_populates="major")
+    count_students: Mapped[int] = mapped_column(server_default=text('0'))
 
     def __str__(self):
-        return f"{self.__class__.__name__}(major_id={self.major_id}, major_name={self.major_name!r})"
+        return f"{self.__class__.__name__}(id={self.id}, major_name={self.major_name!r})"
 
     def __repr__(self):
         return str(self)
